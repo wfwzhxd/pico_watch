2278a9f5e7d206053a73e0665e2227a23a319359
hunter <you@example.com>
2021-08-21 01:31:04 +0800

    删掉没用的代码

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -3,29 +3,15 @@
 #include "disp_drv.h"
 #include "FreeRTOS.h" /* Must come first. */
 #include "task.h"     /* RTOS task related API prototypes. */
-#include "timers.h"   /* Software timer related API prototypes. */
 #include "watch.h"
 #include "semphr.h"
 #include "rtc.h"
 #include <stdio.h>
-#include "input_drv.h"
 
 static char time_str[TIME_LEN_MAX + 1];
 static char date_str[DATE_LEN_MAX + 1];
 
 static lv_obj_t *time, *date;
-
-static volatile uint8_t objChanged = 0;
-
-enum OBJS
-{
-    DATE_OBJ,
-    TIME_OBJ
-};
-
-#define IS_OBJ_CHANGED(OBJ) ((objChanged >> OBJ) & 1)
-#define SET_OBJ_CHANGED(OBJ) (objChanged |= (1 << OBJ))
-#define SET_OBJ_UNCHANGED(OBJ) (objChanged &= ~(1 << OBJ))
 
 static volatile SemaphoreHandle_t xSemaphore;
 
@@ -37,16 +23,17 @@ void create_widgets(void)
 #ifdef DEBUG_PRINTF
     printf("whtfk, create\n");
 #endif
-    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_black(), 0);
+    lv_disp_set_bg_color(lv_disp_get_default(), lv_color_black());
+    lv_obj_t *screen_watch = lv_obj_create(NULL);
+    lv_obj_set_style_bg_opa(screen_watch, LV_OPA_0, 0);
 
     static lv_style_t timestyle;
     lv_style_init(&timestyle);
-    // lv_style_set_bg_color(&timestyle, lv_color_black());
     lv_style_set_text_color(&timestyle, lv_color_white());
     lv_style_set_text_font(&timestyle, &lv_font_kslyt_48);
 
     /*Create a container with ROW flex direction*/
-    time = lv_label_create(lv_scr_act());
+    time = lv_label_create(screen_watch);
     lv_obj_add_style(time, &timestyle, 0);
     lv_label_set_text_fmt(time, "15:13:17");
     lv_obj_set_align(time, LV_ALIGN_TOP_MID);
@@ -56,11 +43,12 @@ void create_widgets(void)
     lv_style_init(&datestyle);
     lv_style_set_text_color(&datestyle, lv_color_white());
     lv_style_set_text_font(&datestyle, &lv_font_kslyt_30);
-    date = lv_label_create(lv_scr_act());
+    date = lv_label_create(screen_watch);
     lv_obj_add_style(date, &datestyle, 0);
     lv_label_set_text_fmt(date, "11月12 星期四");
     lv_obj_set_align(date, LV_ALIGN_BOTTOM_MID);
     lv_obj_set_y(date, -20);
+    lv_disp_load_scr(screen_watch);
 #ifdef DEBUG_PRINTF
     printf("whtfk, end\n");
 #endif
@@ -68,40 +56,30 @@ void create_widgets(void)
 
 void prepare_datetime()
 {
-    static char *daymap[] = {"天", "一", "二", "三", "四", "五", "六"};
+    static const char *daymap[] = {"天", "一", "二", "三", "四", "五", "六"};
     datetime_t *tt = rtc_get_datetime2();
     lv_snprintf(time_str, TIME_LEN_MAX + 1, "%02d:%02d:%02d", tt->hour, tt->min, tt->sec);
-    // printf("rtc time: %s\n", time_str);
     lv_snprintf(date_str, DATE_LEN_MAX, "%d月%02d 星期%s", tt->month, tt->day, daymap[tt->dotw]);
-    // printf("dotw:%d\n", tt->dotw);
-    // printf("rtc date: %s\n", date_str);
 }
 
-void refresh_datetime()
+void refresh_datetime(struct _lv_timer_t * timer)
 {
     prepare_datetime();
     lv_label_set_text(date, date_str);
     // printf("date changed: %s\n", date_str);
     lv_label_set_text(time, time_str);
     // printf("time changed: %s\n", time_str);
+    lv_disp_load_scr(lv_scr_act());
 }
 
 // extern void lv_demo_keypad_encoder(void);
 static void loop()
 {
     uint8_t delay_ms = 50;
-    uint8_t needRefresh = 0;
+    lv_timer_ready(lv_timer_create(refresh_datetime, 100, NULL));
     while (1)
     {
         xSemaphoreTake(lvgl_mutex, portMAX_DELAY);
-        if (2 == needRefresh)
-        {
-            needRefresh = 0;
-            refresh_datetime();
-            // printf("refresh screen\n");
-            lv_disp_load_scr(lv_scr_act());
-        }
-        needRefresh++;
         lv_tick_inc(delay_ms);
         lv_task_handler();
         xSemaphoreGive(lvgl_mutex);
